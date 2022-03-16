54e09fad9d0fe6b66c6e3f2357387b33ac46b41a
Jose <jose.pasqualli@gmail.com>
2022-03-16 17:09:01 -0300

    Configurando o usuario

--- src/br/com/projetoJpa/modelo/basico/Usuario.java
+++ src/br/com/projetoJpa/modelo/basico/Usuario.java
@@ -1,7 +1,14 @@
 package br.com.projetoJpa.modelo.basico;
 
+import javax.persistence.Entity;
+import javax.persistence.Id;
+
+// aqui o entity server para identificar que é uma entidade da JPA
+@Entity
 public class Usuario {
 
+    // aqui refencia qual atributo é o id
+    @Id
     private Long id;
 
     private String nome;
@@ -12,5 +19,32 @@ public class Usuario {
 
     }
 
+    public Usuario(String nome, String email) {
+        this.nome = nome;
+        this.email = email;
+    }
 
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
 }
