c545792fe5ecc91a72e6fb0c9d78ccd6d72cbf17
Jose <jose.pasqualli@gmail.com>
2022-03-16 16:59:22 -0300

    First commit

---
+++ src/br/com/projetoJpa/modelo/basico/Usuario.java
@@ -0,0 +1,16 @@
+package br.com.projetoJpa.modelo.basico;
+
+public class Usuario {
+
+    private Long id;
+
+    private String nome;
+
+    private String email;
+
+    public Usuario() {
+
+    }
+
+
+}
